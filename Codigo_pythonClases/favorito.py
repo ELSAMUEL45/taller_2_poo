f85a5cc2ab7ad759d9ef79fca6bf9ab4b4b59f3b
Jhostin Avendaño Herrera <javendanoh@unal.edu.co>
2026-05-28 08:15:02 -0500

    Actualizar favorito.py
    Se corrigen los errores del docstring previos, se agrega el metodo para comparar 2 favoritos

--- Codigo_pythonClases/favorito.py
+++ Codigo_pythonClases/favorito.py
@@ -7,29 +7,39 @@ class Favorito():
     Clase abstracta que representa la estructura  de un favorito en la aplicacion.
 
     Attributes:
-        usuario (Usuario): Usuario que califica.
-        cancion (Cancion): Correo electrónico con el que se crea la cuenta.
+        cancion (Cancion): Cancion que se agrega a favorito.
         fecha_agregado (str): fecha en la que se agrega una cancion a favoritos 
     """
     
-    def __init__(self, usuario, cancion):
+    def __init__(self, cancion):
         """
         Inicializa los atributos base del usuario.
 
         Args:
-            usuario (Usuario): Usuario que califica.
             cancion (Cancion): Correo electrónico con el que se crea la cuenta.
             fecha_agregado (str)
             
         """
-        self.usuario = usuario
         self.cancion = cancion
         self.fecha = datetime.now().strftime("%Y - %m - %d")
 
     def marcar(self):
+
         pass
     
     def desmarcar(self):
         pass
     
-    
+    def __eq__(self, otro_favorito):
+        """
+        Compara 2 favoritos por medio de las canciones que compara titulo, año y artista
+
+
+        Args:
+            otro_favorito (Favorito): Otro favorito que se desea comparar
+
+
+        Returns:
+            bool: True si ambas canciones de los favoritos coinciden en sus atributos artista, titulo y año 
+        """
+        return self.cancion == otro_favorito.cancion
